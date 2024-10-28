c2fe7e590fd5e34c2cee81b423c9174c529e96eb
artur <lockigod49@gmail.com>
2024-10-29 00:45:05 +0300

    push cal

--- tvm/cell/evaa_proof.go
+++ tvm/cell/evaa_proof.go
@@ -129,7 +129,7 @@ func endExoticCell(b *Builder) *Cell {
 	newCell.calculateHashes()
 
 	if newCell.GetType() == PrunedCellType {
-		newCell.levelMask = LevelMask{Mask: byte(0)}
+		newCell.levelMask = LevelMask{Mask: byte(2)}
 	}
 
 	log.Println("NEW CELL LEVEL", newCell.levelMask)
